9ce1972dfb82f51f0ce2c94f74300c425af07b65
Moto42 <moto42@gmail.com>
2018-09-23 23:38:41 -0500

    minor changes

--- server/router.js
+++ server/router.js
@@ -2,8 +2,8 @@
 //Moved into it's own file to be easy to find and alter
 
 const router = {
-	notFound : require('../lib/handlers/notFound'),
-	sample   : require('../lib/handlers/sample'),
+	'notFound' : require('../lib/handlers/notFound'),
+	'sample'   : require('../lib/handlers/sample'),
 }
 
 module.exports = router;
